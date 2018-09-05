b7c7c20aba8f37eaa87984348477a16e8e8f5d4b
jscasca <jscasca@gmail.com>
2018-09-05 20:09:37 +1000

    Server side translations

--- translations/__es.php
+++ translations/__es.php
@@ -3,6 +3,15 @@ $translations = array(
 	/* Footer */
 	'About us' => 'Conocenos',
 	'Terms and privacy' => 'Terminos y privacidad',
+	/* Building */
+	'This area is still under construction' => 'Esta pagina esta en construccion',
+	'We are sorry for the inconvenience and we are working to have this back online as soon as possible' => 'Lamentamos el inconveniente. Estamos trabajando para estar de vuelta en linea tan pronto como sea posible',
+	'Take me back' => 'Llevame de vuelta al sitio',
+	/* Index */
+	'Join us and discover a new community for readers!' => 'Unete y descubre una nueva comunidad para lectores',
+	'Welcome' => 'Bienvenido',
+	'Start reading the prologues that the community has written!' => 'Empieza a leer los prólogos que la comunidad ha escrito para ti!',
+	'Wishlist what you want to read, share what you are reading and write prologues so other readers can find their next book!' => 'Marca los libros que quieres leer, los que estas leyendo y escribe prólogos para convertirte en un lider de opinión en la comunidad!',
 	/* */
 	'Forgot your password?' => 'Olvidaste tu contraseña?',
 	'Not a member?' => 'Aun no eres miembro?',
@@ -12,8 +21,27 @@ $translations = array(
 	'Sign up!' => 'Registrate!',
 	'Log in' => 'Inicia sesion',
 	'User' => 'Usuario',
+	'Username' => 'Nombre de usuario',
+	'Email' => 'Correo electronico',
 	'Password' => 'Contraseña',
+	'Confirm password' => 'Confirmar contraseña',
+	/* Forgotten password */
+	'New password' => 'Nueva contraseña',
+	'Confirm new password' => 'Confirmar nueva contraseña',
+	'An email has been sent with instructions on how to reset your password!' => 'Se ha enviado un correo electronico con instrucciones para recuperar tu contraseña',
+	'Submit' => 'Enviar',
+	'Forgot your password?' => 'Olvidaste tu contraseña?',
+	'Reset your password' =>'Restablece tu contraseña',
+	'Enter your new password' => 'Escribe tu nueva contraseña',
+	'Email or username' => 'Nombre de usuario o correo',
+	'Enter your username or email address to reset your password' => 'Inserta tu nombre de usuario o correo electronico para cambiar tu contraseña',
+	/* Registration */
+	'Register' => 'Registrate',
+	'Join Prologes by filling this form' => 'Unete a Prologes llenando esta forma',
+	/* Author */
+	'Books from this author' => 'Libros de este autor',
 	/* Navbar */
+	'Find a book' => 'Encuentra un libro',
 	'Search' => 'Buscar',
 	'Profile' => 'Perfil',
 	'Library' => 'Libreria',
