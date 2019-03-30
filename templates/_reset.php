dfcf4367033bdbe8c1cf117497c77fd8f4827179
jscasca <jscasca@gmail.com>
2019-03-30 14:05:04 +1100

    Update reset looks

--- templates/_reset.php
+++ templates/_reset.php
@@ -6,59 +6,62 @@
 <head>
 	<?php
 	include("templates/__header.php");
+	//maybe just styless and favicon?
 	?>
 	<title><?php echo getTranslation('Reset your password'); ?></title>
 </head>
-<body class="body-fullWidth">
-	
-	<section class="Registration text-center">
-		<div class="Login-header">
-			<a href="index.php"><img src="<?php echo $rootpath;?>img/prologes.png" /></a>
-		</div>
-		<div class="Login-container">
-		
-		<?php
-		if(isset($sent) && $sent == true) {
-			echo "<h5>Your password has been updated!</h5>";
-			echo "<a href='login.php'>Log in with your new password</a>";
-		} else {
-		?>
-			<h3><?php echo getTranslation('Reset your password'); ?></h3>
-			<h5><?php echo getTranslation('Enter your new password'); ?></h5>
-			<form action="reset" method="POST" class="Login-form" id="reset--form">
-
+<body class="body-background">
+	<div class="reset forgotten full-body">
+		<section class="centered-section opaque">
+			<div class="header">
+				<a href="index.php"><img src="<?php echo $rootpath;?>img/prologes.png" /></a>
+			</div>
+			<div class="content">
 				<?php
-				if(isset($_REQUEST['token'])) {
-					//print the token
-					echo "<input type='hidden' name='token' id='token' value='".$_REQUEST['token']."' />";
+				if(isset($sent) && $sent === true) {
+				?>
+				<h5><?php echo getTranslation('Your password has been updated!'); ?></h5>
+				<a href='login'><?php echo getTranslation("Log in with your new password");?></a>
+				<?php
+				} else {
+				?>
+				<h2><?php echo getTranslation('Reset your password'); ?></h2>
+
+				<h5><?php echo getTranslation('Enter your new password'); ?></h5>
+				<form action="reset" method="POST" class="Login-form" id="reset--form">
+					<?php
+					if(isset($_REQUEST['token'])) {
+						//print the token
+						echo "<input type='hidden' name='token' id='token' value='".$_REQUEST['token']."' />";
+					}
+					?>
+					<div class="form-group">
+						<div class="input-group">
+							<div class="input-group-addon"><span class="fa fa-lock"></span></div>
+							<input type="password" class="form-control" placeholder="<?php echo getTranslation('New password'); ?>" name="pwd" id="pwd">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group">
+							<div class="input-group-addon"><span class="fa fa-lock"></span></div>
+							<input type="password" class="form-control" placeholder="<?php echo getTranslation('Confirm new password'); ?>" name="pwdCnf" id="pwdCnf">
+						</div>
+					</div>
+					<div id="error-msg" class="error-msg"></div>
+					<button type="send" class="btn main-btn"><?php echo getTranslation('Update password'); ?></button>
+					<h5><?php echo getTranslation('Already have an account?'); ?> <a href="login"><?php echo getTranslation('Log in'); ?></a></h5>
+					<!--<h6>O utiliza tus redes sociales</h6>
+					<div class="Login-buttonContainer">
+						<a href="<?php echo htmlspecialchars($loginUrl); ?>" class="btn Facebook-button">Facebook</a>
+						<a href="#" class="btn Google-button">Google</a>
+					</div>-->
+				</form>
+				<?php
 				}
 				?>
-				<div class="form-group">
-				    <div class="input-group">
-				        <div class="input-group-addon"><span class="fa fa-lock"></span></div>
-				        <input type="password" class="form-control" placeholder="<?php echo getTranslation('New password'); ?>" name="pwd" id="pwd">
-				    </div>
-				</div>
-				<div class="form-group">
-				    <div class="input-group">
-				        <div class="input-group-addon"><span class="fa fa-lock"></span></div>
-				        <input type="password" class="form-control" placeholder="<?php echo getTranslation('Confirm new password'); ?>" name="pwdCnf" id="pwdCnf">
-				    </div>
-				</div>
-				<div id="error-msg"></div>
-				<button type="send" class="btn Basic-button Green-button"><?php echo getTranslation('Submit'); ?></button>
-				<h5><?php echo getTranslation('Already have an account?'); ?> <a href="login.php"><?php echo getTranslation('Log in'); ?></a></h5>
-				<!--<h6>Use your social media logins</h6>
-				<div class="Login-buttonContainer">
-					<a href="<?php echo htmlspecialchars($loginUrl); ?>" class="btn Facebook-button">Facebook</a>
-					<a href="#" class="btn Google-button">Google</a>
-				</div>-->
-			</form>
-		<?php
-		}
-		?>
-		</div>
-	</section>
+			</div>
+		</section>
+	</div>
 
 </body>
 </html>
